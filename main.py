9e9056cfdd1470da7519b2a594743105cba41bc9
RevanDTH <davidkrammer21@gmail.com>
2025-10-22 22:46:58 +0200

    added weather score system

--- main.py
+++ main.py
@@ -7,22 +7,26 @@ root = ttk.Tk()
 
 place_var = ttk.StringVar()
 
-def readPlace():
+def make_prediction():
     place=place_var.get()
     place_resp = json.loads(get_place(place))
     weather = get_weather(str(place_resp["lat"]),str(place_resp["lon"]))
-
-    print(weather)
+    weather_score = analyse_weather(weather)
+    print(weather_score)
+    jacket_advice_tk.config(text=str(weather_score)) ##This is just for debug reasons, I will replace this
     place_var.set("")
 
 title_tk = ttk.Label(text="Wetter-o-Meter")
 
 place_entry_tk = ttk.Entry(root,textvariable=place_var)
 
-submit_button_tk = ttk.Button(root, text="Wetter checken", command=readPlace)
+jacket_advice_tk = ttk.Label(text="-")
+
+submit_button_tk = ttk.Button(root, text="Wetter checken", command=make_prediction)
 
 title_tk.grid(row=0,column=0)
 place_entry_tk.grid(row=1,column=0)
-submit_button_tk.grid(row=2,column=0)
+jacket_advice_tk.grid(row=2,column=0)
+submit_button_tk.grid(row=3,column=0)
 
 root.mainloop()
--- weather_utilities.py
+++ weather_utilities.py
@@ -1,7 +1,11 @@
 import requests as rs
 import json
+import os
+from dotenv import load_dotenv
 
-api_key = "013fca169287728e32de33e9ed823df0"
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
 
 def get_place(place):
     print(place)
@@ -24,6 +28,54 @@ def get_weather(lat,lon):
 
 def analyse_weather(weather_obj):
     weather = json.loads(weather_obj)
-    weather_score = 0 ##Zero is the default score
+    feels_like_weather = float(weather["main"]["feels_like"]) - 273.15
+    weather_score = 0.0 ##Zero is the default score, higher score means better weather
+
+    ##checking main weather state
+    match weather["weather"][0]["main"]:
+        case "clear sky":
+            weather_score += 3
+        case "few clouds":
+            weather_score += 2
+        case "scattered clouds":
+            weather_score += 1
+        case "broken clouds":
+            weather_score += 0
+        case "shower rain":
+            weather_score -= 1
+        case "rain":
+            weather_score -= 2
+        case "thunderstorm":
+            weather_score -= 3
+        case "snow":
+            weather_score -= 4
+        case "mist":
+            weather_score -= 1
 
 
+    ##Some ugly ass if statement, I will rewrite this I promise
+    if feels_like_weather >= 40.0:
+        weather_score += 5
+    elif feels_like_weather >= 35.0 and feels_like_weather <= 39.9:
+        weather_score += 4
+    elif feels_like_weather >= 30.0 and feels_like_weather <= 34.9:
+        weather_score += 3
+    elif feels_like_weather >= 25.0 and feels_like_weather <= 29.9:
+        weather_score += 2
+    elif feels_like_weather >= 20.0 and feels_like_weather <= 24.9:
+        weather_score += 1
+    elif feels_like_weather >= 15.0 and feels_like_weather <= 19.9:
+        weather_score += 0
+    elif feels_like_weather >= 10.0 and feels_like_weather <= 14.9:
+        weather_score -= 1
+    elif feels_like_weather >= 5.0 and feels_like_weather <= 9.9:
+        weather_score -= 2
+    elif feels_like_weather >= 0.0 and feels_like_weather <= 4.9:
+        weather_score -= 3
+    elif feels_like_weather < 0.0:
+        weather_score -= 4
+
+    return weather_score
+
+
+    ## Later on I will add some more stuff to check the weather
